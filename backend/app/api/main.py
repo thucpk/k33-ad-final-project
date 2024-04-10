ef2d4fc3223886a1e7c02f3e04c17043d70f9dd6
Thuc Phan <thuc.phan@timo.vn>
2024-04-10 12:59:18 +0700

    chore: add es autocomplete

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils, search
+from app.api.routes import items, login, users, utils, suggestion, products
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
-api_router.include_router(search.router, prefix="/search", tags=["search"])
+api_router.include_router(suggestion.router, prefix="/suggestion", tags=["search-suggestion"])
+api_router.include_router(products.router, prefix="/products", tags=["products"])
